6063b2be26220fe9005af323b3f3d875377e4571
jorge-andre <jorge.matosandre@gmail.com>
2019-07-29 15:05:04 +0100

    Bug fixing - Backend operational

--- server/routes/shortenurl.js
+++ server/routes/shortenurl.js
@@ -2,49 +2,72 @@ const mongoose = require("mongoose");
 const validUrl = require("valid-url");
 const ShortenUrl = mongoose.model("ShortenUrl");
 const shortid = require("shortid");
-const errorUrl='http://localhost/error';
+const errorUrl = 'http://localhost/error';
+const bodyParser = require("body-parser");
 
 module.exports = app => {
 
-  app.get("/api/item/:code", async (req, res) => {
-    const urlCode = req.params.code;
-    const item = await ShortenUrl.findOne({ urlCode: urlCode });
-    if (item) {
-      return res.redirect(item.originalUrl);
-    } else {
-      return res.redirect(errorUrl);
-    }
-  });
+    app.use(
+        bodyParser.urlencoded({
+            extended: true
+        })
+    );
 
-  app.post("/api/item", async (req, res) => {
-    const { originalUrl, shortBaseUrl } = req.body;
-    if (validUrl.isUri(shortBaseUrl)) {
-    } else {
-      return res
-        .status(401)
-        .json(
-          "Invalid Base Url"
-        );
-    }
-    const updatedAt = new Date();
-    const queryOptions = { originalUrl };
-    if (validUrl.isUri(originalUrl)) {
-      try {
-        
-          const urlCode = shortCode.generate();
-          shortUrl = shortBaseUrl + '/' + urlCode;
-          const itemToBeSaved = { originalUrl, shortUrl, urlCode, updatedAt };
+    app.use(bodyParser.json());
 
-          // Add the item to db
-          const item = new UrlShorten(itemToBeSaved);
-          await item.save();
-          res.status(200).json(itemToBeSaved);
-        
-      } catch (err) {
-        res.status(401).json('Invalid User Id');
-      }
-    } else {
-      return res.status(401).json('Invalid Original Url.');
-    }
-  });
+    app.get("/api/item/:code", async(req, res) => {
+        const urlCode = req.params.code;
+        const item = await ShortenUrl.findOne({ urlCode: urlCode });
+        if (item) {
+            return res.redirect(item.originalUrl);
+        } else {
+            return res.redirect(errorUrl);
+        }
+    });
+
+    app.post("/api/item", async(req, res) => {
+        const originalUrl = req.body.originalUrl;
+        const shortBaseUrl = req.body.shortBaseUrl;
+
+        if (validUrl.isUri(shortBaseUrl)) {} else {
+            return res
+                .status(401)
+                .json(
+                    "Invalid Base Url"
+                );
+        }
+
+        const urlCode = shortid.generate();
+        const updatedAt = new Date();
+
+        if (validUrl.isUri(originalUrl)) {
+            try {
+                const item = await ShortenUrl.findOne({ originalUrl: originalUrl });
+                if (item) {
+                    res.status(200).json(item);
+                } else {
+                    console.log("beep");
+                    shortUrl = shortBaseUrl + "/" + urlCode;
+                    const item = new ShortenUrl({
+                        originalUrl,
+                        shortUrl,
+                        urlCode,
+                        updatedAt
+                    });
+                    await item.save();
+                    res.status(200).json(item);
+                }
+
+            } catch (err) {
+                res.status(401).json("Invalid User Id");
+            }
+
+        } else {
+            return res
+                .status(401)
+                .json(
+                    "Invalid Original Url"
+                );
+        }
+    });
 };
